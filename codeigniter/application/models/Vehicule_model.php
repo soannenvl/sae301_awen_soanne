318b5a6ec5232d9adb616c7dedb892acc106d0ad
AwenLeng <115996398+AwenLeng@users.noreply.github.com>
2022-10-24 12:43:54 +0200

    test

--- codeigniter/application/models/Vehicule_model.php
+++ codeigniter/application/models/Vehicule_model.php
@@ -5,5 +5,10 @@ Class Vehicule_model extends CI_Model{
         $query = $this->db->query("SELECT * FROM vehicule");
         return $query->result_array();
     }
+    function autrefonction(){
+        $this->load->database();
+        $query = $this->db->query("add vehicule");
+        return $query->result_array();
+    }
 }
 ?>
--- codeigniter/application/views/veh_view.php
+++ codeigniter/application/views/veh_view.php
@@ -1,5 +1,33 @@
 <?php 
-print_r($vehArray); 
-$marque0=$vehArray[0]['marque'];
-$modele0=$vehArray[0]['modele'];
+foreach ($vehArray as $key => $value){
+$id =           $vehArray[$key]['id'];
+$type_vehicule =$vehArray[$key]['type_vehicule'];
+$kilometrage =  $vehArray[$key]['kilometrage'];
+$nb_places =    $vehArray[$key]['nb_places'];
+$marque =       $vehArray[$key]['marque'];
+$modele =       $vehArray[$key]['modele'];
+$puissance =    $vehArray[$key]['puissance'];
+
+    echo("<div>");
+        echo("<div>Type de véhicule: ".$type_vehicule."</div>");
+        echo("<div>Kilométrage: ".$kilometrage."</div>");
+        echo("<div>Nombre de places: ".$nb_places."</div>");
+        echo("<div>Marque: ".$marque."</div>");
+        echo("<div>Modele: ".$modele."</div>");
+        echo("<div>Puissance chevaux: ".$puissance."</div>");
+        echo("<br>");
+    echo("</div>");
+}
 ?>
+
+<html>
+<head>
+    <title>vehicule list</title>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
