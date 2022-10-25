51aae5e551042f29422d013472d287ec351d34ed
soannenvl <soanne.nouvel24@orange.fr>
2022-10-25 16:06:54 +0200

    on essaye

--- codeigniter/application/views/register.php
+++ codeigniter/application/views/register.php
@@ -2,49 +2,56 @@
 <html>
 <head>
     <title>Inscription</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" />
 </head>
 <body>
    
-<div class="container">
+<div class="container_inscription">
+<h1> Créer un compte </h1>
         <?php echo form_open('User/Register'); ?>
         <div class="form-group">
             <label for="email">Email address</label>
-            <input type="email" class="form-control" value="<?php echo set_value('email'); ?>" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" value="<?php echo set_value('email'); ?>" id="email" name="email" aria-describedby="emailHelp" placeholder="Entrer votre e-mail">
             <?php echo form_error('email'); ?>
         </div>
+
         <div class="form-group">
             <label for="Login">Login</label>
-            <input type="text" class="form-control" value="<?php echo set_value('Login'); ?>" name="Login" placeholder="Enter Login">
+            <input type="text" class="form-control" value="<?php echo set_value('Login'); ?>" name="Login" placeholder="Entrer un nom d'utilisateur">
             <?php echo form_error('Login'); ?>
         </div>
+
         <div class="form-group">
             <label for="Nom">Nom</label>
-            <input type="text" class="form-control" value="<?php echo set_value('Nom'); ?>" name="Nom" placeholder="Enter Name">
+            <input type="text" class="form-control" value="<?php echo set_value('Nom'); ?>" name="Nom" placeholder="Entrer votre nom">
             <?php echo form_error('Nom'); ?>
         </div>
+
         <div class="form-group">
             <label for="Prenom">Prenom</label>
-            <input type="text" class="form-control" value="<?php echo set_value('Prenom'); ?>" name="Prenom" placeholder="Enter Prenom">
+            <input type="text" class="form-control" value="<?php echo set_value('Prenom'); ?>" name="Prenom" placeholder="Entrer votre prénom">
             <?php echo form_error('Prenom'); ?>
         </div>
+
         <div class="form-group">
             <label for="ddn">Date de naissance</label>
-            <input type="date" class="form-control" value="<?php echo set_value('ddn'); ?>" name="ddn" placeholder="Enter ddn">
+            <input type="date" class="form-control" value="<?php echo set_value('ddn'); ?>" name="ddn" placeholder="Entrer votre date de naissance">
             <?php echo form_error('ddn'); ?>
         </div>
         
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Créer un mot de passe">
             <?php echo form_error('password'); ?>
         </div>
+
         <div class="form-group">
             <label for="verif_password">Verification Password</label>
-            <input type="password" class="form-control" value="<?php echo set_value('verif_password'); ?>" name="verif_password" placeholder="Enter verif_password">
+            <input type="password" class="form-control" value="<?php echo set_value('verif_password'); ?>" name="verif_password" placeholder="Recopier le mot de passe">
             <?php echo form_error('verif_password'); ?>
         </div>
 
-        <button type="submit" class="btn btn-primary">Register</button>
+        <button type="submit" class="submit">Créer</button>
         <?php echo form_error('login_error'); ?>
         <?php form_close(); ?>
     </div>
